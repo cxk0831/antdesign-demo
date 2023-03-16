c1d5178c6e751eb2563cffea47b065e1940022a2
chenxiaokun <chenxiaokun@didiglobal.com>
2023-03-16 14:28:39 +0800

    ✨ feat: 模拟场景
    模拟场景

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback } from "react";
+import "./App.css";
+// 此 antd 已经设置了 @ant-prefix 为 antd3
+import "./antd.css";
+import { Modal, Button, ConfigProvider } from "antd";
 
 function App() {
+  const onClick = useCallback(() => {
+    Modal.confirm({
+      title: 'hello world！',
+      // 弹窗样式前缀
+      prefixCls: 'antd3-modal',
+      // 确定按钮样式前缀
+      okButtonProps: {
+        prefixCls: 'antd3-btn'
+      },
+      // 取消按钮样式前缀
+      cancelButtonProps: {
+        prefixCls: 'antd3-btn'
+      }
+    })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider prefixCls="antd3">
+      <div className="App">
+        <Button type="primary" size="large" onClick={onClick}>请点击按钮</Button>
+      </div>
+    </ConfigProvider>
   );
 }
 
